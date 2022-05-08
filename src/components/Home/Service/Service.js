76644ce94f858973b0a55eaad7f98789ddc7a3b3
unknown <shufolislam1255@gmail.com>
2022-05-08 21:03:16 +0600

    see all button done and 20% remaining

--- src/components/Home/Service/Service.js
+++ src/components/Home/Service/Service.js
@@ -5,7 +5,6 @@ import SingelService from "../SingelService/SingelService";
 import "./Service.css";
 
 const Service = () => {
-  const [pageCount, setPageCount] = useState(0);
   const [service, setService] = useState([]);
 
   
@@ -15,22 +14,12 @@ const Service = () => {
     .then((data) => setService(data));
   }, []);
   
-  useEffect( () => {
-    fetch('http://localhost:5000/stockCount')
-    .then(res => res.json())
-    .then(data => {
-      const count = data.count;
-      const pages = Math.ceil(count/6)
-      setPageCount(pages)
-    })
-  } , [])
   const minService = service.slice(0, 6);
   const navigate = useNavigate();
 
   const handleMoreStocks = () => {
-    navigate('/morestocks')
+    navigate('/manageitems')
   }
-  // console.log(minService);
   return (
     <div>
       <h1 className="text-dark">Stocks</h1>
@@ -42,9 +31,6 @@ const Service = () => {
           ></SingelService>
         ))}
 
-        {/* {
-          service?.map(moreService => <MoreStocks key={moreService.id} moreService={moreService}></MoreStocks>)
-        } */}
       </div>
       <Button onClick={ () => handleMoreStocks()} className="my-4" variant="dark">See More Stocks</Button>
     </div>
--- src/components/ManageItems/ManageItems.js
+++ src/components/ManageItems/ManageItems.js
@@ -1,34 +1,53 @@
-import React from 'react';
-import useServices from '../../hooks/useServices';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import useServices from "../../hooks/useServices";
+import "./ManageItem.css";
 
 const ManageItems = () => {
+  const [service, setService] = useServices();
 
-    const [service, setService] = useServices()
-
-    const handleDelete = id => {
-        const proceed = window.confirm('Are you sure ?')
-        if(proceed){
-            const url = `http://localhost:5000/stock/${id}`
-            fetch(url, {
-                method: 'DELETE'
-            })
-            .then(res => res.json())
-            .then(data => {
-                const remaining = service.filter(serviceOne => serviceOne._id !== id)
-                setService(remaining)
-            })
-        }
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are you sure ?");
+    if (proceed) {
+      const url = `http://localhost:5000/stock/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const remaining = service.filter(
+            (serviceOne) => serviceOne._id !== id
+          );
+          setService(remaining);
+        });
     }
-    return (
-        <div>
-            <h2>this is manageitems</h2>
-            {
-                service.map(serviceOne => <div key={serviceOne._id}>
-                    <h4>{serviceOne.name} <button onClick={() => handleDelete(serviceOne._id)}>Delete</button></h4>
-                </div>)
-            }
-        </div>
-    );
+  };
+  return (
+    <div>
+      <h2 className="text-dark mx-auto mt-2"> Manageitems</h2>
+      <div className="service-style">
+        {service.map((serviceOne) => (
+          <div key={serviceOne._id}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={serviceOne.img} />
+              <Card.Body>
+                <Card.Title>
+                  <h4>{serviceOne.name}</h4>
+                </Card.Title>
+                <Card.Text>{serviceOne.description}</Card.Text>
+                <Button
+                  onClick={() => handleDelete(serviceOne._id)}
+                  variant="dark"
+                >
+                  Delete
+                </Button>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ManageItems;
